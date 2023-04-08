f50ec011266714017faab2af5fb8879335ec9c19
Petar Vasiljevic <petarvasiljevic2001@gmail.com>
2023-04-08 16:52:09 +0200

    Added binary operation parsing

---
+++ backend/parser.py
@@ -0,0 +1,19 @@
+def eval_binaryop(eval_dict : dict):
+    operand_1 = eval_dict['operand1']
+    operand_2 = eval_dict['operand2']
+
+    op_code = eval_dict['operation']
+
+    evaluated_operand_1 = eval(operand_1)
+    evaluated_operand_2 = eval(operand_2)
+
+    if op_code == '+':
+        return evaluated_operand_1 + evaluated_operand_2
+    elif op_code == '-':
+        return evaluated_operand_1 - evaluated_operand_2
+    elif op_code == '*':
+        return evaluated_operand_1 * evaluated_operand_2
+    elif op_code == '/':
+        return evaluated_operand_1 / evaluated_operand_2
+    else:
+        raise Exception("eval_binaryop : not supported operation")
