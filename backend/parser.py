1101fa8dc9cd3699919a3fee2f8d552ee0097757
ivangogic <ivangogic@protonmail.com>
2023-04-09 00:35:45 +0200

    Added malloc operation and free operation

--- backend/parser.py
+++ backend/parser.py
@@ -1,5 +1,10 @@
 memory = {}
 names = {}
+heap_memory = {}
+stack_pos = 1000
+stack_size = 1000
+heap_pos = 2000
+heap_size = 1000
 
 
 def eval_expr(data):
@@ -12,7 +17,9 @@ def eval_expr(data):
         'const': eval_const,
         'variable': eval_variable,
         'while': eval_while,
-        'if': eval_if
+        'if': eval_if,
+        'malloc': eval_malloc,
+        'free': eval_free
     }
     if type(data) == list:
         for instruction in data:
@@ -112,7 +119,7 @@ def eval_declare(data):
     if data['name'] in names.keys():
         return -1
 
-    for i in range(100,1000):
+    for i in range(stack_pos, stack_pos + stack_size):
         if i not in memory.keys():
             memory[i] = None
             if 'init' in data:
@@ -149,3 +156,40 @@ def eval_while(instr):
             memory.pop(names[local_variable][0])
             names.pop(local_variable)
 
+
+def eval_malloc(instr):
+    # print(instr)
+    space = []
+    n_instr = instr['n']
+    n = eval_expr(n_instr)
+    for i in range(heap_pos, heap_pos + heap_size):
+        if i not in memory.keys():
+            space.append(i)
+            if len(space) == n:
+                break
+        else:
+            space.clear()
+
+    # print(space)
+    if len(space) == n:
+        for pos in space:
+            memory[pos] = 0
+        heap_memory[space[0]] = n
+        return space[0]
+    else:
+        return -1
+
+
+def eval_free(instr):
+    print(instr)
+    var_name = instr['name']
+    var_addr = memory[names[var_name][0]]
+    print(var_addr)
+    print(memory)
+    if var_addr in heap_memory.keys():
+        n = heap_memory.get(var_addr)
+        for i in range(var_addr, var_addr + n):
+            memory.pop(i)
+        heap_memory.pop(var_addr)
+    else:
+        return -1
